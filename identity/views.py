39987616cfca5e3ed32b8dfe1f659afd52a786b7
nowhereOnce <alejandro.aguilar.fi02@gmail.com>
2026-03-18 17:19:48 -0600

    Added a new view and dynamic template. This template shows the status of the request in real time.

--- identity/views.py
+++ identity/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import IdentityUploadForm
 from .tasks import process_ocr_task
+from .models import VerificationRequest
 
 @login_required
 def upload_identity(request):
@@ -16,7 +17,13 @@ def upload_identity(request):
             # DISPARAMOS LA TAREA ASÍNCRONA
             process_ocr_task.delay(str(obj.id))
             
-            return render(request, 'identity/success.html', {'request_id': obj.id})
+            return redirect('verification_detail', pk=obj.id)
     else:
         form = IdentityUploadForm()
     return render(request, 'identity/upload.html', {'form': form})
+
+@login_required
+def verification_detail(request, pk):
+    # Aseguramos que solo el dueño de la solicitud pueda verla
+    verification = get_object_or_404(VerificationRequest, pk=pk, user=request.user)
+    return render(request, 'identity/detail.html', {'verification': verification})
